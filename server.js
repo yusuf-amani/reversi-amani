960b017180bc5b52c279a8cf5889b4e41b226e73
Yusuf Amani <yamani@uci.edu>
2021-06-06 00:12:25 -0700

    added the flip tokens functionality

--- server.js
+++ server.js
@@ -599,11 +599,13 @@ io.on('connection', (socket) => {
         /* Execute the move */
         if (color === 'white'){
             game.board[row][column] = 'w';
+            flip_tokens('w', row, column, game.board);
             game.whose_turn = 'black';
             game.legal_moves = calculate_legal_moves('b', game.board);
         }
         else if (color === 'black'){
             game.board[row][column] = 'b';
+            flip_tokens('b', row, column, game.board);
             game.whose_turn = 'white';
             game.legal_moves = calculate_legal_moves('w', game.board);
         }
@@ -742,6 +744,46 @@ function calculate_legal_moves(who,board){
     }
     return legal_moves; 
 }
+
+
+function flip_line(who, dr, dc, r, c, board){
+    if((r + dr < 0) || (r + dr > 7)){
+        return false; 
+    }
+    if((c + dc < 0) || (c + dc > 7)){
+        return false; 
+    }
+    if(board[r+dr][c+dc] === ' '){
+        return false; 
+    }
+    if(board[r+dr][c+dc] === who){
+        return true; 
+    }
+    else{
+        if(flip_line(who, dr, dc, r+dr, c+dc, board)){
+            board[r+dr][c+dc] = who;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
+function flip_tokens(who, row, column, board){
+    flip_line(who,-1,-1, row, column, board);
+    flip_line(who,-1,0, row, column, board);
+    flip_line(who,-1,1, row, column, board);
+
+    flip_line(who,0,-1, row, column, board);
+    flip_line(who,0,1, row, column, board);
+
+    flip_line(who,1,-1, row, column, board);
+    flip_line(who,1,0, row, column, board);
+    flip_line(who,1,1, row, column, board);
+}
+
 
 
 function send_game_update(socket, game_id, message ){
